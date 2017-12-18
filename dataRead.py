77843156a46d6270df5ccde1123ad07929672d93
Bala93 <balamuralim.1993@gmail.com>
2017-12-18 12:59:42 +0530

    Read data and convert to pytorch model

--- dataRead.py
+++ dataRead.py
@@ -13,24 +13,39 @@ from torch.utils.data import DataLoader,TensorDataset
 
 def get_data():
     
-    path = '/media/htic/NewVolume1/murali/ecg/codes/datasets/data_with_labels.npy'
-    dataset_npy = np.load(path)
-    dataset_x  = dataset_npy[:,:360].astype(np.float32)
-    dataset_y  = dataset_npy[:,360:].astype(np.int)
-    count = np.unique(dataset_y,return_counts = True)
-    print count
     
-    in_data     = torch.FloatTensor(dataset_x)
-    out_labels  = torch.IntTensor(dataset_y)
+    train_path = '/media/htic/NewVolume1/murali/ecg/codes/datasets/train_data.npy'
+    test_path  = '/media/htic/NewVolume1/murali/ecg/codes/datasets/test_data.npy'
+    feature_length = 360
     
     
-    dset    = TensorDataset(in_data,out_labels)
-    dloader = DataLoader(dset)
+    train_npy  = np.load(train_path)
+    test_npy   = np.load(test_path)
 
-    return dloader
-#for x,y in dloader:
-#    print x.size(),y.size()
-#    print x
-#    break
+    train_x    = train_npy[:,:feature_length].astype(np.float32)
+    train_y    = train_npy[:,feature_length:].astype(np.int)    
 
-get_data()
+    test_x     = test_npy[:,:feature_length].astype(np.float32)
+    test_y     = test_npy[:,feature_length:].astype(np.int)
+
+    train_x_torch = torch.FloatTensor(train_x)
+    train_y_torch = torch.IntTensor(train_y)
+
+    test_x_torch = torch.FloatTensor(test_x)
+    test_y_torch = torch.IntTensor(test_y)
+    
+    
+    train_dataset = TensorDataset(train_x_torch,train_y_torch)
+    train_loader  = DataLoader(train_dataset)
+
+
+    return train_loader,test_x_torch,test_y_torch
+
+#
+#if __name__ == "__main__":
+#    train_loader,test_x,test_y  = get_data()
+#    for (x,y) in train_loader:
+#        print x.size(),y.size()
+#        break
+#    print test_x.size(),test_y.size()
+    
